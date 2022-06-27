8ffa4d94b2c7ed5024b2070189e744231271477c
maykaltenev <michaeltenev@gmail.com>
2022-06-27 21:46:26 +0200

    project 2 starter

--- 22_Day_Manipulating_DOM_object/22_day_starters/project_2/scripts/main.js
+++ 22_Day_Manipulating_DOM_object/22_day_starters/project_2/scripts/main.js
@@ -1,1 +1,13 @@
-console.log(countries)
+// console.log(countries)
+
+
+let country = document.getElementsByClassName("countries-wrapper");
+
+
+for (let i = 0; i <= countries.length; i++) {
+    console.log(countries[i])
+    div = document.createElement('div');
+    console.log(div)
+    div.innerText = countries[i];
+    country.appendChild(div)
+}
